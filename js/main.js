6ea6148503fa3af1903ffbe62ca70f8b93948018
ImranNayan <39517042+ImranNayan@users.noreply.github.com>
2022-01-27 17:31:13 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 window.onload = () => { 'use strict';
 if ('serviceWorker' in navigator) { navigator.serviceWorker.register('./sw.js')} }
+
+navigator.getBattery().then(b => { document.querySelector('#bty').innerHTML = b.level * 100 +'%'})
