222406bb31a42a24ee9fc849ab0b50b2cc11da70
ImranNayan <39517042+ImranNayan@users.noreply.github.com>
2022-01-28 22:41:09 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 window.onload = () => { 'use strict';
 if ('serviceWorker' in navigator) { navigator.serviceWorker.register('./sw.js')} }
 
-setInterval(myTimer, 1000)
-function myTimer() {
-document.querySelector('#time').innerHTML = new Date().toLocaleTimeString();
-}
-document.querySelector('#date').innerHTML = (new Date()).toString().split(' ').splice(1,3).join(' ');
+
